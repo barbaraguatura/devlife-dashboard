ec1f79f592de04d2392d02aaca19285530d96399
Yago Molina <yagooandrademolinaa@gmail.com>
2026-09-02 16:12:15 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="bg-slate-900 text-white px-8 py-4 font-bold">
+            <h1>Devlife Dashboard</h1>
+        </header>
+    )
+}
+export default Header;
+
+// HEADER
